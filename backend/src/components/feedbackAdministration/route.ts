dd8dac2b78ec12692a138dc28b364f56525ba142
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-10-08 18:45:56 +0800

    use plural route

--- backend/src/components/feedbackAdministration/route.ts
+++ backend/src/components/feedbackAdministration/route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { feedbackAdministrationController as controller } from "./controller";
 
 export const feedbackAdministrationRouter = Router();
-const baseUrl = "/feedback"
+const baseUrl = "/feedbacks"
 
 feedbackAdministrationRouter.get(`${baseUrl}`, controller.list)
--- backend/src/components/feedbackSubmission/route.ts
+++ backend/src/components/feedbackSubmission/route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { feedbackSubmissionController as controller } from "./controller";
 
 export const feedbackSubmissionRouter = Router();
-const baseUrl = "/feedback"
+const baseUrl = "/feedbacks"
 
 feedbackSubmissionRouter.post(`${baseUrl}/submit`, controller.submit)

4e8173b2d619e1aa31c051c821ac5017a5350c4d
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-10-08 20:32:22 +0800

    analyze messages in kafka

--- sentiment-analysis/kafka_consumer.py
+++ sentiment-analysis/kafka_consumer.py
@@ -1,6 +1,7 @@
 
 from json import loads
 from kafka import KafkaConsumer
+from feedback_analyzer import get_sentiment
 
 consumer = KafkaConsumer('feedback-submitted',
                          bootstrap_servers=['localhost:9092'],
@@ -15,8 +16,21 @@ while True:
             for consumer_record in consumer_records:
                 id = consumer_record.key.decode('utf-8')
                 feedback = consumer_record.value.decode('utf-8')
+                score = get_sentiment(feedback)
+
                 print(id)
                 print(feedback)
+                print(score)
+
+                category = "neutral"
+
+                if score > 0.2:
+                    category = "positive"
+                if score <= -0.2:
+                    category = "negative"
+
+                print(category)
+
         continue
     except Exception as e:
         print(e)

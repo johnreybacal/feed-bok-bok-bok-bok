e424027fcd46cd0166694d68c1ad98fee4ca20c2
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-10-08 21:50:15 +0800

    sequelize test env

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -1,21 +1,23 @@
 import { Dialect, Sequelize } from "sequelize";
 
-const dialect = process.env.DB_ENGINE! as Dialect
+const isTest = (process.env.NODE_ENV || "development") === "test";
+let dialect = (isTest ? process.env.TEST_DB_ENGINE! : process.env.DB_ENGINE!) as Dialect
+let sqlitePath = isTest ? process.env.TEST_SQLITE_PATH! : process.env.SQLITE_PATH!
 
 export const sequelize = dialect === "sqlite" ?
     new Sequelize(
         {
             dialect,
-            storage: process.env.SQLITE_PATH
+            storage: sqlitePath
         }
     )
     : new Sequelize(
-        process.env.DB_NAME!,
-        process.env.DB_USERNAME!,
-        process.env.DB_PASSWORD,
+        isTest ? process.env.TEST_DB_NAME! : process.env.DB_NAME!,
+        isTest ? process.env.TEST_DB_USERNAME! : process.env.DB_USERNAME!,
+        isTest ? process.env.TEST_DB_PASSWORD! : process.env.DB_PASSWORD!,
         {
-            dialect: process.env.DB_ENGINE! as Dialect,
-            host: process.env.DB_HOST!,
-            port: process.env.DB_PORT! as unknown as number
+            dialect,
+            host: isTest ? process.env.TEST_DB_HOST! : process.env.DB_HOST!,
+            port: (isTest ? process.env.TEST_DB_PORT! : process.env.DB_PORT!) as unknown as number
         }
     );

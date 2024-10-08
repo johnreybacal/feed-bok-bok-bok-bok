a41c1c00c525b2a89a363356bbfae62d8bf3ff1c
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-10-08 18:46:21 +0800

    add admin prefix

--- backend/src/components/feedbackAdministration/route.ts
+++ backend/src/components/feedbackAdministration/route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { feedbackAdministrationController as controller } from "./controller";
 
 export const feedbackAdministrationRouter = Router();
-const baseUrl = "/feedbacks"
+const baseUrl = "/admin/feedbacks"
 
 feedbackAdministrationRouter.get(`${baseUrl}`, controller.list)

cac1790a8a666f6c9096cf12efe7951c8bb58212
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-10-09 20:44:51 +0800

    setup backend unit test

---
+++ backend/test/formSubmission.service.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it, jest } from "@jest/globals";
+import { Feedback } from "../src/components/feedback/model";
+import { feedbackSubmissionService } from "../src/components/feedbackSubmission/service";
+
+jest.mock('sequelize');
+
+describe('FeedbackSubmissionService', () => {
+    describe('save', () => {
+        it('should create a new feedback', async () => {
+            const mockFeedback = {
+                name: 'John Doe',
+                email: 'john.doe@example.com',
+                feedback: 'This is a great product',
+            };
+
+            jest.spyOn(Feedback, 'create').mockResolvedValueOnce(mockFeedback);
+
+            const result = await feedbackSubmissionService.save(
+                mockFeedback.name,
+                mockFeedback.email,
+                mockFeedback.feedback
+            );
+
+            setTimeout(() => {
+                expect(result.id).not.toBeNull();
+                expect(result.name).toBe(mockFeedback.name)
+                expect(result.email).toBe(mockFeedback.email)
+                expect(result.feedback).toBe(mockFeedback.feedback)
+                expect(result.category).toBeNull()
+            }, 100);
+        });
+    });
+});
